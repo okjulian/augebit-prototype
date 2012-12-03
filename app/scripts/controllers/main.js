725abf7163fadb8c42c1c565fcd6f3d6c879b14f
Julian Mayorga <mayorga.julian@gmail.com>
2012-12-02 23:09:59 -0300

    Add signup form that sends user email to augeBIT account in parse.com

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
 augebitPrototypeApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+  $scope.save = function() {
+    if (emailForm.$invalid === false) {
+      Parse.initialize('lywqlwHwaizR1HHTv1dYQtzSrkDJRkArdI3QfMpR', '5bpXBKHHfhD4QESUr4LYQI1kYaxvniTvXycrdB8l');
+
+      var EmailObject = Parse.Object.extend('EmailObject');
+      var emailObject = new EmailObject();
+      var email = $scope.userEmail;
+      emailObject.save({email: email});
+    }
+  };
+
+  $scope.isSaveDisabled = function() {
+    return $scope.emailForm.$invalid;
+  };
 });
